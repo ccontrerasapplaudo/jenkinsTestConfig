1d090b1f55082c4043422589f6a0148ebe79bdd9
Carlos Rafael Contreras <103429472+carloscontreras0297@users.noreply.github.com>
2022-10-01 21:00:28 -0600

    Parametrized browser

--- src/test/java/com/example/jenkinstestconfig/FirstSeleniumJavaTest.java
+++ src/test/java/com/example/jenkinstestconfig/FirstSeleniumJavaTest.java
@@ -10,6 +10,8 @@ import org.testng.annotations.Test;
 
 public class FirstSeleniumJavaTest {
     private WebDriver driver;
+    String browser = System.getProperty("browser");
+
 
     @Test
     public void testEasy(){
@@ -21,9 +23,42 @@ public class FirstSeleniumJavaTest {
     }
 
     @BeforeTest
-    public void beforeTest(){
-        driver = WebDriverManager.firefoxdriver().create();
+    public void beforeTest() {
+        if (browser != "" && browser != null) {
+            if (browser.equalsIgnoreCase("chrome")) {
+                driver = WebDriverManager.chromedriver().create();
+            }
+            else if (browser.equalsIgnoreCase("firefox")) {
+                driver = WebDriverManager.firefoxdriver().create();
+            }
+            else if (browser.equalsIgnoreCase("edge")) {
+                driver = WebDriverManager.edgedriver().create();
+            }
+            else {
+                System.out.println("Invalid option Selected hence defaulting to chrome");
+                driver = WebDriverManager.chromedriver().create();
+            }
+        }
+        else {
+            browser = "Chrome";
+            driver = WebDriverManager.chromedriver().create();
+        }
     }
+
+
+
+//        switch(browser) {
+//            case "edge":
+//                driver = WebDriverManager.edgedriver().create();
+//                break;
+//            case "firefox":
+//                driver = WebDriverManager.firefoxdriver().create();
+//                break;
+//            default:
+//                driver = WebDriverManager.chromedriver().create();
+//        }
+
+
 
     @AfterTest
     public void afterTest(){
