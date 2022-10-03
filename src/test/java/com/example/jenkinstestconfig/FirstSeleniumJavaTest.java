edb975d11f3268d70ee8e60da7f6067860fca2a9
Carlos Rafael Contreras <103429472+carloscontreras0297@users.noreply.github.com>
2022-10-03 09:46:04 -0600

    Simple demo with loc.gov redirect

--- src/test/java/com/example/jenkinstestconfig/FirstSeleniumJavaTest.java
+++ src/test/java/com/example/jenkinstestconfig/FirstSeleniumJavaTest.java
@@ -14,7 +14,7 @@ public class FirstSeleniumJavaTest {
     @Test
     public void testEasy() throws InterruptedException {
         driver.manage().window().maximize();
-        driver.get("https://google.com.sv");
+        driver.get("https://www.loc.gov/");
         waitTime();
 
         String expectedURL="https://www.loc.gov/";
